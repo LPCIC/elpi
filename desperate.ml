09cfd2c13e9d4e3fb9d1cfc4c644e64a2f91daa5
Enrico Tassi <gares@fettunta.org>
2015-04-10 17:27:03 +0200

    version 2, the hack is faster and less ugly

--- desperate.ml
+++ desperate.ml
@@ -13,53 +13,48 @@ type term =
   (* Pure terms *)
   | Const of string
   (* Query terms *)
-  | Struct of term array
+  | Struct of term list
   | Arg of int
   (* Heap terms *)
-  | App of term array
-  | UVar of term array * int (* and eventually extra metadata like the level *)
+  | App of term list
+  | UVar of term ref
 
-(* A term cannot be just UVar,  if so it is a degenerate App [| UVar(..) |] *)
+let dummy = App []
 
 let ppterm f t =
   let rec ppapp a c1 c2 = 
     Format.fprintf f "%c@[<hov 1>" c1;
-    let last = Array.length a - 1 in
-    Array.iteri (fun j x -> aux a x; if j <> last then Format.fprintf f "@ ") a;
+    List.iter (fun x -> aux x; Format.fprintf f "@ ") a;
     Format.fprintf f "@]%c" c2
-  and aux a = function
+  and aux = function
     | App a -> ppapp a '(' ')'
     | Struct a -> ppapp a '{' '}'
-    | UVar (a',i) ->
-         if a' == a then Format.fprintf f "(%d)" i
-         else Format.fprintf f "%d+%d" (Obj.magic a') i
+    | UVar { contents = t } -> aux t
     | Const s -> Format.fprintf f "%s" s
     | Arg i -> Format.fprintf f "A%d" i
   in
-    aux [||] t
+    aux t
 ;;
 
-let undef a i = match a.(i) with UVar(b,j) -> b == a && i == j | _ -> false
+type key = string * string
+
+type clause = { hd : term; hyps : term list; vars : int; key : key }
 
 let dummyk = "dummy"
 
 let key_of = function
   | (App xs | Struct xs) -> begin
-      match xs.(0), xs.(1) with
-      | Const w, (App ys | Struct ys) -> begin
-          match ys.(0) with
-          | Const z -> w,z
+      match xs with
+      | Const w :: (App ys | Struct ys) :: _ -> begin
+          match ys with
+          | Const z :: _ -> w,z
           | _ -> w, dummyk
           end
-      | Const w, Const z -> w,z
-      | Const w, _ -> w,dummyk
+      | Const w :: Const z :: _ -> w,z
+      | Const w :: _ -> w,dummyk
       | _ -> dummyk, dummyk
   end
   | _ -> dummyk, dummyk
-
-type key = string * string
-
-type clause = { hd : term; hyps : term list; vars : int; key : key }
 
 let clause_match_key (j1,j2) { key = (k1,k2) } =
   j1 == dummyk || k1 == dummyk ||
@@ -67,44 +62,20 @@ let clause_match_key (j1,j2) { key = (k1,k2) } =
 
 (* The environment of a clause and stack frame *)
 
-let mk_env size =
-  let a = Array.make size (Arg (-1)) in
-  Array.iteri (fun i _ -> a.(i) <- UVar (a,i)) a;
-  a
+let mk_env size = Array.create size dummy
 
 let to_heap e t =
-  let rec every = function
-    | [] -> ()
-    | t :: ts -> every (each t ts)
-  and each a todo =
-    let len = Array.length a in
-    let todo = ref todo in
-    for j = 0 to len - 1 do
-      match a.(j) with
-      | (Const _ | UVar _ | App _) -> ()
-      | Struct b ->
-          let c = Array.copy b in
-          todo := c :: !todo;
-          a.(j) <- App c 
-      | Arg i when undef e i ->
-          let v = UVar(a,j) in
-          a.(j) <- v;
-          e.(i) <- v
-      | Arg i ->
-          match e.(i) with
-          | (Const _ | UVar _ | App _) as x -> a.(j) <- x
-          | Struct b ->
-               let c = Array.copy b in
-               todo := c :: !todo;
-               let v = App c in
-               a.(j) <- v;
-               e.(i) <- v
-          | Arg _ -> assert false
-    done;
-    !todo in
-  let res = [|t|] in
-  every [res];
-  res.(0)
+  let rec aux = function
+    | (Const _ | UVar _ | App _) as x -> x (* heap term *)
+    | Struct b -> App (List.map aux b)
+    | Arg i ->
+        let a = e.(i) in
+        if a == dummy then
+            let v = UVar(ref dummy) in
+            e.(i) <- v;
+            v
+        else aux a
+  in aux t
 ;;
 
 type frame = {
@@ -130,56 +101,44 @@ let trail_this (trail,no_alternative) old =
 
 (* Invariant: LSH is a heap term *)
 let unif trail e_a e_b a b =
- let rec unif a b todo =
+ let rec unif a b =
    (* Format.eprintf "unif: %a = %a\n%!" ppterm a ppterm b; *)
    a == b || match a,b with
    | (Arg _ | Struct _), _ -> assert false
-   | _, Arg i when not (undef e_b i) -> unif a e_b.(i) todo
-   | UVar(a,i), _ when not (undef a i) -> unif a.(i) b todo
-   | _, UVar(b,i) when not (undef b i) -> unif a b.(i) todo
+   | _, Arg i when e_b.(i) != dummy -> unif a e_b.(i)
+   | UVar { contents = t }, _ when t != dummy -> unif t b
+   | _, UVar { contents = t } when t != dummy -> unif a t
    | UVar _, Arg j -> e_b.(j) <- a; true
-   | t, Arg i -> trail_this trail e_b.(i); e_b.(i) <- t; true
-   | UVar(a,n) as old, t -> trail_this trail old; a.(n) <- to_heap e_b t; true
-   | t, (UVar(a,n) as old) -> trail_this trail old; a.(n) <- t; true
+   | t, Arg i -> trail_this trail (`Arr (e_b,i)); e_b.(i) <- t; true
+   | UVar r, t -> trail_this trail (`Ref r); r := to_heap e_b t; true
+   | t, UVar r -> trail_this trail (`Ref r); r := t; true
    | Const x, Const y -> x == y (* !!! hashcons the entire Const node *)
-   | App xs, (Struct ys | App ys) -> todo := (xs,ys) :: !todo; true
+   | App xs, (Struct ys | App ys) -> List.for_all2 unif xs ys
    | Const _, (App _ | Struct _) -> false
    | App _, Const _  -> false in
- let todo = ref [] in
- let b = ref (unif a b todo) in
- let more = ref (!todo != []) in
- while !b && !more do
-   match !todo with
-   | [] -> more := false
-   | (xs, ys) :: rest ->
-      todo := rest;
-      let len = Array.length xs in
-      b := len = Array.length ys;
-      let i = ref 0 in
-      while !b && !i < len do
-        let cur = !i in
-        b := unif xs.(cur) ys.(cur) todo;
-        incr i;
-      done;
- done;
- !b
+ unif a b
 ;;
 
 (* Backtracking *)
 
+type trail = [ `Arr of term array * int | `Ref of term ref ]
+
 type alternative = {
   stack : frame;
-  trail : term list;
+  trail : trail list;
   clauses : clause list
 }
 
 let undo_trail old_trail trail =
   while !trail != old_trail do
     match !trail with
-    | UVar(a,i) :: rest -> a.(i) <- UVar(a,i); trail := rest
+    | `Ref r :: rest -> r := dummy; trail := rest
+    | `Arr(a,i) :: rest -> a.(i) <- dummy; trail := rest
     | _ -> assert false
   done
 ;;
+
+(* Loop *)
 
 let put_all_args f = { f with
    goals = List.map (fun (_,x) ->
@@ -244,60 +203,55 @@ let ak = "a"
 let bk = "b"
 
 (* Program *)
-let app1 = { hd = Struct[| Const appk; Const nilk; Arg 0; Arg 0 |]; hyps = []; vars = 1; key = (appk,nilk) };;
-let app2 = { hd = Struct[| Const appk; Struct[|Const consk; Arg 0; Arg 1|]; Arg 2; Struct [|Const consk; Arg 0; Arg 3 |] |]; hyps = [ Struct [| Const appk; Arg 1; Arg 2; Arg 3|] ]; vars = 4; key = (appk,consk) };;
+let app1 = { hd = Struct[ Const appk; Const nilk; Arg 0; Arg 0 ]; hyps = []; vars = 1; key = (appk,nilk) };;
+let app2 = { hd = Struct[ Const appk; Struct[Const consk; Arg 0; Arg 1]; Arg 2; Struct [Const consk; Arg 0; Arg 3 ] ]; hyps = [ Struct [ Const appk; Arg 1; Arg 2; Arg 3] ]; vars = 4; key = (appk,consk) };;
 
-let rev1 = { hd = Struct[| Const revk; Const nilk; Arg 0; Arg 0 |]; hyps = []; vars = 1; key = (revk,nilk) };;
-let rev2 = { hd = Struct[| Const revk; Struct[| Const consk; Arg 0; Arg 1|]; Arg 2; Arg 3 |];
-             hyps = [Struct[|Const revk; Arg 1; Struct [| Const consk; Arg 0; Arg 2|]; Arg 3|]];
+let rev1 = { hd = Struct[ Const revk; Const nilk; Arg 0; Arg 0 ]; hyps = []; vars = 1; key = (revk,nilk) };;
+let rev2 = { hd = Struct[ Const revk; Struct[ Const consk; Arg 0; Arg 1]; Arg 2; Arg 3 ];
+             hyps = [Struct[Const revk; Arg 1; Struct [ Const consk; Arg 0; Arg 2]; Arg 3]];
              vars = 4; key = (revk,consk) };;
-let refl = { hd = Struct[| Const eqk; Arg 0; Arg 0|]; hyps = []; vars = 1; key = (eqk,dummyk) };;
-
-(*
-let l1 =
-   App [| Const consk; Const ak; App [| Const consk; Const bk; Const nilk |]|];; 
-let gs =
-  let rec a1 = [|Const appk; l1; l1; UVar(a1,3)|] in
-  let rec aR1 = [|Const revk; UVar(a1,3); Constnilk; UVar(aR1,3)|] in
-  let rec aR2 = [|Const revk; UVar(aR1,3); Constnilk; UVar(aR2,3)|] in
-  [
-          App a1;
-          App aR1;
-          App aR2;
-          App [|Const eqk; UVar(a1,3);UVar(aR2,3)|] 
-  ];;
-
-let p = [app1;app2;rev1;rev2;refl];;
-let rec top = { lvl = 0; env = [||]; goals = gs; next = top; };;
-run p top ;;
-
-exit 0;;
-*)
+let refl = { hd = Struct[ Const eqk; Arg 0; Arg 0]; hyps = []; vars = 1; key = (eqk,dummyk) };;
 
 let l1 =
-   App [| Const consk; Const ak; App [| Const consk; Const bk; 
-   App [| Const consk; Const ak; App [| Const consk; Const bk; 
-   App [| Const consk; Const ak; App [| Const consk; Const bk; 
-   App [| Const consk; Const ak; App [| Const consk; Const bk; 
-   App [| Const consk; Const ak; App [| Const consk; Const bk; 
-   Const nilk |]|]|]|]|]|]|]|]|]|];;
+   App [ Const consk; Const ak; App [ Const consk; Const bk; 
+   App [ Const consk; Const ak; App [ Const consk; Const bk; 
+   App [ Const consk; Const ak; App [ Const consk; Const bk; 
+   App [ Const consk; Const ak; App [ Const consk; Const bk; 
+   App [ Const consk; Const ak; App [ Const consk; Const bk; 
+   Const nilk ]]]]]]]]]];;
 let gs =
-  let rec a1 = [|Const appk; l1; l1; UVar(a1,3)|] in
-  let rec a2 = [|Const appk; UVar(a1,3); UVar(a1,3); UVar(a2,3)|] in
-  let rec a3 = [|Const appk; UVar(a2,3); UVar(a2,3); UVar(a3,3)|] in
-  let rec a4 = [|Const appk; UVar(a3,3); UVar(a3,3); UVar(a4,3)|] in
-  let rec a5 = [|Const appk; UVar(a4,3); UVar(a4,3); UVar(a5,3)|] in
-  let rec a6 = [|Const appk; UVar(a5,3); UVar(a5,3); UVar(a6,3)|] in
-  let rec a7 = [|Const appk; UVar(a6,3); UVar(a6,3); UVar(a7,3)|] in
-  let rec a8 = [|Const appk; UVar(a7,3); UVar(a7,3); UVar(a8,3)|] in
-  let rec a9 = [|Const appk; UVar(a8,3); UVar(a8,3); UVar(a9,3)|] in
-  let rec a10 = [|Const appk; UVar(a9,3); UVar(a9,3); UVar(a10,3)|] in
-  let rec a11 = [|Const appk; UVar(a10,3); UVar(a10,3); UVar(a11,3)|] in
-  let rec a12 = [|Const appk; UVar(a11,3); UVar(a11,3); UVar(a12,3)|] in
-  let rec a13 = [|Const appk; UVar(a12,3); UVar(a12,3); UVar(a13,3)|] in
-  let rec a14 = [|Const appk; UVar(a13,3); UVar(a13,3); UVar(a14,3)|] in
-  let rec aR1 = [|Const revk; UVar(a14,3); Const nilk; UVar(aR1,3)|] in
-  let rec aR2 = [|Const revk; UVar(aR1,3); Const nilk; UVar(aR2,3)|] in
+  let v1 = UVar (ref dummy) in
+  let v2 = UVar (ref dummy) in
+  let v3 = UVar (ref dummy) in
+  let v4 = UVar (ref dummy) in
+  let v5 = UVar (ref dummy) in
+  let v6 = UVar (ref dummy) in
+  let v7 = UVar (ref dummy) in
+  let v8 = UVar (ref dummy) in
+  let v9 = UVar (ref dummy) in
+  let v10 = UVar (ref dummy) in
+  let v11 = UVar (ref dummy) in
+  let v12 = UVar (ref dummy) in
+  let v13 = UVar (ref dummy) in
+  let v14 = UVar (ref dummy) in
+  let r1 = UVar (ref dummy) in
+  let r2 = UVar (ref dummy) in
+  let a1 = [Const appk; l1; l1; v1] in
+  let a2 = [Const appk; v1; v1; v2] in
+  let a3 = [Const appk; v2; v2; v3] in
+  let a4 = [Const appk; v3; v3; v4] in
+  let a5 = [Const appk; v4; v4; v5] in
+  let a6 = [Const appk; v5; v5; v6] in
+  let a7 = [Const appk; v6; v6; v7] in
+  let a8 = [Const appk; v7; v7; v8] in
+  let a9 = [Const appk; v8; v8; v9] in
+  let a10 = [Const appk; v9; v9; v10] in
+  let a11 = [Const appk; v10; v10; v11] in
+  let a12 = [Const appk; v11; v11; v12] in
+  let a13 = [Const appk; v12; v12; v13] in
+  let a14 = [Const appk; v13; v13; v14] in
+  let aR1 = [Const revk; v14; Const nilk; r1] in
+  let aR2 = [Const revk; r1; Const nilk; r2] in
   [
           App a1;
           App a2;
@@ -315,7 +269,7 @@ let gs =
           App a14;
           App aR1;
           App aR2;
-          App [|Const eqk; UVar(a14,3);UVar(aR2,3)|] 
+          App [Const eqk; v14;r2] 
   ];;
 
 let p = [app1;app2;rev1;rev2;refl];;
