0d96a34e4becfda596e2f9b86cc157cc43822a73
Ferruccio Guidi <fguidi@maelstrom.helm.cs.unibo.it>
2016-07-27 16:01:13 +0200

    we finally fixed ocaml dependencies to compile matita with elpi

--- matita/components/ng_kernel/nCicELPI.ml
+++ matita/components/ng_kernel/nCicELPI.ml
@@ -2,4 +2,4 @@ module LP = Elpi_runtime
 
 let _ =
    Printf.printf "LP.register_custom\n%!";
-   LP.register_custom "pippo" (fun ~depth:_ ~env:_ _ ts -> ts)
+   LP.register_custom "$pippo" (fun ~depth:_ ~env:_ _ ts -> ts)
