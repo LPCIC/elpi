fc0229ebfd00a8590be1844cbb33250ae2ec1ffa
Ferruccio Guidi <fguidi@maelstrom.helm.cs.unibo.it>
2016-10-11 22:18:28 +0200

    Barendregt convention assured in CIC to ELPI translation
    Barendregt convention is not guaranteed in CIC terms,
    especially when they are generated by auto.
    Thus, local reference by CIC name leads to buggy ELPI terms.
    now the name of the translated local reference includes its level.
    
    CIC local names are now forgotten, so translation is faster.

--- matita/components/ng_kernel/nCicELPI.ml
+++ matita/components/ng_kernel/nCicELPI.ml
@@ -76,19 +76,19 @@ let mk_nil = LPA.mkNil
 
 let mk_cons v vs = LPA.mkSeq [v; vs]
 
-let mk_lref c i =
-   try LPA.mkCon (List.nth c (pred i))
-   with Not_found -> assert false
+let mk_name i = Printf.sprintf "x%u" i
+
+let mk_lref c i = LPA.mkCon (mk_name (c - i))
 
 let mk_gref r = LPA.mkCon (R.string_of_reference r)
 
 let mk_sort s = LPA.mkApp [LPA.mkCon "sort"; mk_univ s]
 
-let mk_prod n w t = LPA.mkApp [LPA.mkCon "prod"; w; LPA.mkLam n t]
+let mk_prod n w t = LPA.mkApp [LPA.mkCon "prod"; w; LPA.mkLam (mk_name n) t]
 
-let mk_abst n w t = LPA.mkApp [LPA.mkCon "abst"; w; LPA.mkLam n t]
+let mk_abst n w t = LPA.mkApp [LPA.mkCon "abst"; w; LPA.mkLam (mk_name n) t]
 
-let mk_abbr n v w t = LPA.mkApp [LPA.mkCon "abst"; v; w; LPA.mkLam n t]
+let mk_abbr n v w t = LPA.mkApp [LPA.mkCon "abst"; v; w; LPA.mkLam (mk_name n) t]
 
 let mk_appl t vs = LPA.mkApp [LPA.mkCon "appl"; t; vs]
 
@@ -107,9 +107,9 @@ let rec lp_term c = function
    | C.Rel i               -> mk_lref c i
    | C.Const r             -> mk_gref r
    | C.Sort s              -> mk_sort s
-   | C.Prod (n, w, t)      -> mk_prod n (lp_term c w) (lp_term (n::c) t)
-   | C.Lambda (n, w, t)    -> mk_abst n (lp_term c w) (lp_term (n::c) t)
-   | C.LetIn (n, w, v, t)  -> mk_abbr n (lp_term c v) (lp_term c w) (lp_term (n::c) t)
+   | C.Prod (_, w, t)      -> mk_prod c (lp_term c w) (lp_term (succ c) t)
+   | C.Lambda (_, w, t)    -> mk_abst c (lp_term c w) (lp_term (succ c) t)
+   | C.LetIn (_, w, v, t)  -> mk_abbr c (lp_term c v) (lp_term c w) (lp_term (succ c) t)
    | C.Appl (t :: vs)      -> mk_appl (lp_term c t) (lp_terms c vs)
    | C.Appl []             -> assert false
    | C.Match (r, u, v, ts) -> mk_case (mk_gref r) (lp_term c v) (lp_term c u) (lp_terms c ts)
@@ -119,7 +119,7 @@ and lp_terms c = function
    | v :: vs -> mk_cons (lp_term c v) (lp_terms c vs)
 
 let mk_term ~depth t =
-   LPR.term_of_ast ~depth (lp_term [] t)
+   LPR.term_of_ast ~depth (lp_term 0 t)
 
 let mk_int ~depth i =
    LPR.term_of_ast ~depth (LPA.mkInt i)
@@ -213,9 +213,9 @@ let execute r query =
    Printf.printf "%s %s\n%!" result str; b
 
 let has_some_sort r u =
-   let query = mk_has_some_sort (lp_term [] u) in
+   let query = mk_has_some_sort (lp_term 0 u) in
    execute r query
 
 let has_type r t u =
-   let query = mk_has_type (lp_term [] t) (lp_term [] u) in
+   let query = mk_has_type (lp_term 0 t) (lp_term 0 u) in
    execute r query
