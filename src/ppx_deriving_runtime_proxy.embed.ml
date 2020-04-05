fc982924f70d673bc6deb6c4a2d6b655e76f9eb1
Enrico Tassi <Enrico.Tassi@Inria.fr>
2020-04-05 15:23:53 +0200

    minimal deps: take re and ditch ppx_deriving.runtime
    Apparently vendoring for a library stanza does not work

---
+++ src/ppx_deriving_runtime_proxy.embed.ml
@@ -0,0 +1,8 @@
+type nonrec int = int
+type nonrec char = char
+type nonrec string = string
+type nonrec float = float
+type nonrec bool = bool
+type nonrec unit = unit
+module Format = Format
+let compare = Pervasives.compare
