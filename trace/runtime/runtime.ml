0ad8390c6c02069a86d97b6b42f2659c0431ba46
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-06-28 10:08:26 +0200

    trace: fix json encoding

--- trace/runtime/runtime.ml
+++ trace/runtime/runtime.ml
@@ -215,10 +215,61 @@ let pp_a fmt (l : j list) =
   end;
   F.fprintf fmt "]"
 
+
+module JSON_STRING_ENCODING = struct
+  (* This code is from Yojson *)
+
+  let hex n =
+    Char.chr (
+      if n < 10 then n + 48
+      else n + 87
+    )
+  
+  let write_special src start stop ob str =
+    Buffer.add_substring ob src !start (stop - !start);
+    Buffer.add_string ob str;
+    start := stop + 1
+  
+  let write_control_char src start stop ob c =
+    Buffer.add_substring ob src !start (stop - !start);
+    Buffer.add_string ob "\\u00";
+    Buffer.add_char ob (hex (Char.code c lsr 4));
+    Buffer.add_char ob (hex (Char.code c land 0xf));
+    start := stop + 1
+  
+  let finish_string src start ob =
+    try
+      Buffer.add_substring ob src !start (String.length src - !start)
+    with exc ->
+      Printf.eprintf "src=%S start=%i len=%i\n%!"
+        src !start (String.length src - !start);
+      raise exc
+  
+  let write_string_body ob s =
+    let start = ref 0 in
+    for i = 0 to String.length s - 1 do
+      match s.[i] with
+          '"' -> write_special s start i ob "\\\""
+        | '\\' -> write_special s start i ob "\\\\"
+        | '\b' -> write_special s start i ob "\\b"
+        | '\012' -> write_special s start i ob "\\f"
+        | '\n' -> write_special s start i ob "\\n"
+        | '\r' -> write_special s start i ob "\\r"
+        | '\t' -> write_special s start i ob "\\t"
+        | '\x00'..'\x1F'
+        | '\x7F' as c -> write_control_char s start i ob c
+        | _ -> ()
+    done;
+    finish_string s start ob
+
+end
+
 let pp_as fmt (l : j list) =
   let pp_j fmt x =
     let s = F.asprintf "%a" pp_j x in
-    F.fprintf fmt "%S" s in
+    let b = Buffer.create 64 in
+    JSON_STRING_ENCODING.write_string_body b s;
+    F.fprintf fmt "\"%s\"" (Buffer.contents b) in
   F.fprintf fmt "[";
   begin match l with
   | [] -> ()
