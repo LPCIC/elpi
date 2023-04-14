4cd61256532631c6a4271153e6153a0673356203
Kiran Gopinathan <kirang@pop-os.localdomain>
2023-04-14 17:18:39 +0200

    updated driver code to use latest API

--- ocaml-elpi/main_ocaml_elpi_rewriter.ml
+++ ocaml-elpi/main_ocaml_elpi_rewriter.ml
@@ -4,7 +4,33 @@ open Ocaml_ast_for_elpi
 let builtin = let open BuiltIn in
   declare ~file_name:(Sys.argv.(1)) parsetree_declaration
 
-let mapper = String.concat "\n" (Elpi.Builtin.PPX.mapper_src :: parsetree_mapper)
+let mapper_src = {|
+namespace ppx {
+
+pred map.list i:(A -> B -> prop), i:list A, o:list B.
+map.list _ [] [].
+map.list F [X|XS] [Y|YS] :- F X Y, map.list F XS YS.
+
+pred map.option i:(A -> B -> prop), i:option A, o:option B.
+map.option _ none none.
+map.option F (some X) (some Y) :- F X Y.
+
+pred map.pair i:(A1 -> B1 -> prop), i:(A2 -> B2 -> prop), i:pair A1 A2, o:pair B1 B2.
+map.pair F1 F2 (pr X1 X2) (pr Y1 Y2) :- F1 X1 Y1, F2 X2 Y2.
+
+pred map.triple i:(A1 -> B1 -> prop), i:(A2 -> B2 -> prop), i:(A3 -> B3 -> prop), i:triple A1 A2 A3, o:triple B1 B2 B3.
+map.triple F1 F2 F3 (triple X1 X2 X3) (triple Y1 Y2 Y3) :- F1 X1 Y1, F2 X2 Y2, F3 X3 Y3.
+
+pred map.quadruple i:(A1 -> B1 -> prop), i:(A2 -> B2 -> prop), i:(A3 -> B3 -> prop), i:(A4 -> B4 -> prop), i:quadruple A1 A2 A3 A4, o:quadruple B1 B2 B3 B4.
+map.quadruple F1 F2 F3 F4 (quadruple X1 X2 X3 X4) (quadruple Y1 Y2 Y3 Y4) :- F1 X1 Y1, F2 X2 Y2, F3 X3 Y3, F4 X4 Y4.
+
+pred map.quintuple i:(A1 -> B1 -> prop), i:(A2 -> B2 -> prop), i:(A3 -> B3 -> prop), i:(A4 -> B4 -> prop), i:(A5 -> B5 -> prop), i:quintuple A1 A2 A3 A4 A5, o:quintuple B1 B2 B3 B4 B5.
+map.quintuple F1 F2 F3 F4 F5 (quintuple X1 X2 X3 X4 X5) (quintuple Y1 Y2 Y3 Y4 Y5) :- F1 X1 Y1, F2 X2 Y2, F3 X3 Y3, F4 X4 Y4, F5 X5 Y5.
+
+}
+|}
+
+let mapper = String.concat "\n" (mapper_src :: parsetree_mapper)
 
 let program_src = ref ""
 let typecheck = ref false
@@ -20,18 +46,19 @@ ocaml-elpi.ppx: no program specified. Supported options:
 |};
     exit 1;
   end;
-  let elpi, _ = Setup.init ~builtins:[builtin;Elpi.Builtin.std_builtins] ~basedir:"." [] in
+  let elpi = Setup.init ~builtins:[builtin;Elpi.Builtin.std_builtins]  () in
   BuiltIn.document_file builtin;
   if !debug then
-    Setup.trace ["-trace-on";"tty";"stderr";"-trace-only";"user";"-trace-only-pred";"map";"-trace-at";"run";"1";"99999"];
-  let program = Parse.program ~elpi [!program_src] in
-  let mapper = Parse.program_from_stream ~elpi (Ast.Loc.initial "mapper") (Stream.of_string mapper) in
+    ignore @@ Setup.trace ["-trace-on";"tty";"stderr";"-trace-only";"user";"-trace-only-pred";"map";"-trace-at";"run";"1";"99999"];
+  let program = Parse.program ~elpi ~files:[!program_src] in
+  let mapper =
+    Parse.program_from ~elpi ~loc:(Ast.Loc.initial "mapper") (Lexing.from_string mapper) in
   let program = Compile.program ~elpi ~flags:Compile.default_flags [program;mapper] in
   let query =
     let open Query in
     let open ContextualConversion in
     compile program (Ast.Loc.initial "ppx") @@
-      Query { predicate = "map.structure"; arguments = D(!< structure,s,(Q(!< structure,"Result",N))) } in
+      Query { predicate = "map.structure"; arguments = D(structure,s,(Q(structure,"Result",N))) } in
   if !typecheck then begin
     if not @@ Compile.static_check ~checker:Elpi.Builtin.(default_checker ()) query then begin
       exit 1
@@ -57,7 +84,7 @@ let erase_loc =
 let expression_quotation ~depth state _loc s =
   let e = Ppxlib.Parse.expression (Lexing.from_string s) in
   let e, state = erase_loc#expression e state in
-  let state, x, gls = (ContextualConversion.(!<) expression).Conversion.embed ~depth state e in
+  let state, x, gls = (expression).Conversion.embed ~depth state e in
   assert(gls = []);
   state, x
 
@@ -67,7 +94,7 @@ let () = Quotation.set_default_quotation expression_quotation
 let pattern_quotation ~depth state _loc s =
   let e = Ppxlib.Parse.pattern (Lexing.from_string s) in
   let e, state = erase_loc#pattern e state in
-  let state, x, gls = (ContextualConversion.(!<) pattern).Conversion.embed ~depth state e in
+  let state, x, gls = (pattern).Conversion.embed ~depth state e in
   assert(gls = []);
   state, x
 
@@ -76,7 +103,7 @@ let () = Quotation.register_named_quotation ~name:"pat" pattern_quotation
 let type_quotation ~depth state _loc s =
   let e = Ppxlib.Parse.core_type (Lexing.from_string s) in
   let e, state = erase_loc#core_type e state in
-  let state, x, gls = (ContextualConversion.(!<) core_type).Conversion.embed ~depth state e in
+  let state, x, gls = (core_type).Conversion.embed ~depth state e in
   assert(gls = []);
   state, x
 
@@ -87,7 +114,7 @@ let stri_quotation ~depth state _loc s =
   match e with
   | Ptop_def [e] ->
       let e, state = erase_loc#structure_item e state in
-      let state, x, gls = (ContextualConversion.(!<) structure_item).Conversion.embed ~depth state e in
+      let state, x, gls = (structure_item).Conversion.embed ~depth state e in
       assert(gls = []);
       state, x
   | Ptop_def _ ->
@@ -102,7 +129,7 @@ let sigi_quotation ~depth state _loc s =
   match e with
   | [e] ->
       let e, state = erase_loc#signature_item e state in
-      let state, x, gls = (ContextualConversion.(!<) signature_item).Conversion.embed ~depth state e in
+      let state, x, gls = (signature_item).Conversion.embed ~depth state e in
       assert(gls = []);
       state, x
   | _ ->
@@ -113,7 +140,7 @@ let () = Quotation.register_named_quotation ~name:"sigi" stri_quotation
 let structure_quotation ~depth state _loc s =
   let e = Ppxlib.Parse.implementation (Lexing.from_string s) in
   let e, state = erase_loc#structure e state in
-  let state, x, gls = (ContextualConversion.(!<) structure).Conversion.embed ~depth state e in
+  let state, x, gls = (structure).Conversion.embed ~depth state e in
   assert(gls = []);
   state, x
 
@@ -122,7 +149,7 @@ let () = Quotation.register_named_quotation ~name:"str" structure_quotation
 let signature_quotation ~depth state _loc s =
   let e = Ppxlib.Parse.interface (Lexing.from_string s) in
   let e, state = erase_loc#signature e state in
-  let state, x, gls = (ContextualConversion.(!<) signature).Conversion.embed ~depth state e in
+  let state, x, gls = (signature).Conversion.embed ~depth state e in
   assert(gls = []);
   state, x
 
