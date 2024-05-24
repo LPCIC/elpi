b8a8b77638409c3faa6e955c218dd7f694c2ccdc
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-05-24 10:40:05 +0200

    fix trace

--- src/runtime/runtime.ml
+++ src/runtime/runtime.ml
@@ -4288,6 +4288,7 @@ let make_runtime : ?max_steps: int -> ?delay_outside_fragment: bool -> executabl
         in
       prune alts in
     if cutto_alts == Noalts then T.trail := T.empty ();
+    [%spy "user:rule:cut" ~rid ~gid pp_string "success"];
     match gs with
     | [] -> pop_andl cutto_alts next cutto_alts
     | { depth; program; goal; gid = gid [@trace] } :: gs -> run depth program goal (gid[@trace]) gs next cutto_alts cutto_alts
