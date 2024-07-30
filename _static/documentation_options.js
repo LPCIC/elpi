ef32542c75e818835a8f6232a3767d948e260f88
gares <Enrico.Tassi@inria.fr>
2024-07-30 14:41:10 +0000

    Deploying to gh-pages from @ LPCIC/elpi@d0bbea5bd86a494ac13db1d8b9f67ce9461f824c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.19.5',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
