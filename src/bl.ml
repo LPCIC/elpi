b7986d1a3334cea5078829867fd8e5dc7d5086d9
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-09-10 10:28:00 +0200

    cleanup bl

--- src/bl.ml
+++ src/bl.ml
@@ -1,33 +1,46 @@
+(* elpi: embedded lambda prolog interpreter                                  *)
+(* license: GNU Lesser General Public License Version 2.1 or later           *)
+(* ------------------------------------------------------------------------- *)
 
-  type 'a t = BNil | BCons of { mutable head : 'a; mutable last : 'a t; mutable tail : 'a t }
-  let rec pp pp_a fmt = function
+type 'a t = BNil | BCons of { mutable head : 'a; mutable last : 'a t; mutable tail : 'a t }
+
+let rec pp pp_a fmt = function
   | BNil -> Format.fprintf fmt "[]"
   | BCons { head; tail; last } as x ->
       Format.fprintf fmt "[self %x, tail %x, last %x] %a :: "
-        (Obj.magic x    land 0xffffff)
+        (Obj.magic x land 0xffffff)
         (Obj.magic tail land 0xffffff)
         (Obj.magic last land 0xffffff)
-        pp_a head ; pp pp_a fmt tail
-  let show pp_a x = Format.asprintf "%a" (pp pp_a) x
-  type  'a l = Nil | Cons of { head : 'a; last : unit; tail : 'a l }
-  [@@deriving show, ord]
+        pp_a head;
+      pp pp_a fmt tail
 
-  let empty () = BNil
+let show pp_a x = Format.asprintf "%a" (pp pp_a) x
+
+type 'a l = Nil | Cons of { head : 'a; last : unit; tail : 'a l } [@@deriving show, ord]
+
+let empty () = BNil
+
 let cons head tail =
   match tail with
-  | BNil -> let rec last = BCons { head; tail; last } in last
+  | BNil ->
+      let rec last = BCons { head; tail; last } in
+      last
   | BCons { last } -> BCons { head; tail; last }
 
 let set_last_tail l = function
- | BCons x -> assert(x.tail = BNil);
- (* Format.eprintf "set tail of %d to %d \n" (Obj.magic w) (Obj.magic l);  *)
- x.tail <- l
- | BNil -> Elpi_util.Util.anomaly "blist: can't rcons after commit"
+  | BCons x ->
+      assert (x.tail = BNil);
+      (* Format.eprintf "set tail of %d to %d \n" (Obj.magic w) (Obj.magic l);  *)
+      x.tail <- l
+  | BNil -> Elpi_util.Util.anomaly "blist: can't rcons after commit"
 
 let rcons head tail =
   match tail with
-  | BNil -> let rec last = BCons { head; tail = BNil; last } in last
-  | BCons b -> let rec last = BCons { head; tail = BNil; last } in
+  | BNil ->
+      let rec last = BCons { head; tail = BNil; last } in
+      last
+  | BCons b ->
+      let rec last = BCons { head; tail = BNil; last } in
       let olast = b.last in
       b.last <- last;
       set_last_tail last olast;
@@ -40,7 +53,7 @@ let rec replace f x = function
 
 let rec insert_before f x = function
   | BNil -> assert false (*let rec last = BCons { head = x; last; tail = BNil } in last*)
-  | BCons { head; last } as l when f head -> BCons { head = x; last; tail = l}
+  | BCons { head; last } as l when f head -> BCons { head = x; last; tail = l }
   | BCons { head; last; tail } -> BCons { head; last; tail = insert_before f x tail }
 
 let insert_after f x = function
@@ -49,23 +62,24 @@ let insert_after f x = function
       let rec insert_after_aux = function
         | BNil -> assert false
         | BCons ({ head; last; tail } as b) when f head ->
-           let new_tail = BCons { head = x; last; tail } in
-           if tail == BNil then b0.last <- new_tail;     
-           b.tail <- new_tail
+            let new_tail = BCons { head = x; last; tail } in
+            if tail == BNil then b0.last <- new_tail;
+            b.tail <- new_tail
         | BCons { tail } -> insert_after_aux tail
       in
-        insert_after_aux l
+      insert_after_aux l
+
 let commit l = Obj.magic l
 
 let commit_map f l : 'b l =
   let rec commit = function
     | BNil -> ()
-    | BCons ({ head; last; tail } as x) -> x.head <- Obj.magic @@ f head; x.last <- BNil; commit tail
+    | BCons ({ head; last; tail } as x) ->
+        x.head <- Obj.magic @@ f head;
+        x.last <- BNil;
+        commit tail
   in
   commit l;
   Obj.magic l
-let rec to_list = function
-  | Nil -> []
-  | Cons { head; tail } -> head :: to_list tail
 
-
+let rec to_list = function Nil -> [] | Cons { head; tail } -> head :: to_list tail
--- src/bl.mli
+++ src/bl.mli
@@ -1,16 +1,34 @@
+(* elpi: embedded lambda prolog interpreter                                  *)
+(* license: GNU Lesser General Public License Version 2.1 or later           *)
+(* ------------------------------------------------------------------------- *)
+
+(* These lists become functional only after a call to commit. Before that
+   they are imperative, keeping a pointer to an old "copy" must be avoided.
+   Before calling commit rcons is O(1) time and space. *)
+
 type 'a t
 val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
 val show : (Format.formatter -> 'a -> unit) -> 'a t -> string
 
-type  'a l = Nil | Cons of { head : 'a; last : unit; tail : 'a l }
-[@@deriving show, ord]
-
+(* These 3 are O(1) *)
 val empty : unit -> 'a t
 val cons : 'a -> 'a t -> 'a t
 val rcons : 'a -> 'a t -> 'a t
+
+(* These 3 are O(n) time, O(1) space. The test must succeed once *)
 val replace : ('a -> bool) -> 'a -> 'a t -> unit
 val insert_before : ('a -> bool) -> 'a -> 'a t -> 'a t
 val insert_after : ('a -> bool) -> 'a -> 'a t -> unit
+
+type  'a l = Nil | Cons of { head : 'a; last : unit; tail : 'a l }
+val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a l -> unit
+val show : (Format.formatter -> 'a -> unit) -> 'a l -> string
+
+(* no more rcons, O(1) time and space *)
 val commit : 'a t -> 'a l
+
+(* no more rcons, O(n) time, O(1) space *)
 val commit_map : ('a -> 'b) -> 'a t -> 'b l
+
+(* allocates a new list, O(n) time and space *)
 val to_list : 'a l -> 'a list
