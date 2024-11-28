8e7a9ead91a38f9de4b27b7973c813c4bc86a98c
Davide Fissore <fisso9900@gmail.com>
2024-11-28 13:34:18 +0100

    [spilling] add header to mli

--- src/compiler/spilling.mli
+++ src/compiler/spilling.mli
@@ -1,1 +1,5 @@
+(* elpi: embedded lambda prolog interpreter                                  *)
+(* license: GNU Lesser General Public License Version 2.1 or later           *)
+(* ------------------------------------------------------------------------- *)
+
 val main : Compiler_data.ScopedTerm.t -> Compiler_data.ScopedTerm.t
