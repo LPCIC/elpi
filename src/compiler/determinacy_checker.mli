e740cb1adffcdec76c09c86b225c1184fd6ba505
Davide Fissore <fisso9900@gmail.com>
2025-03-18 15:30:17 +0100

    cleanup env in DetCheck

--- src/compiler/determinacy_checker.mli
+++ src/compiler/determinacy_checker.mli
@@ -6,16 +6,14 @@ open Elpi_util.Util
 
 type t [@@deriving show, ord]
 
-val empty : t
+val empty_env : t
 val merge : t -> t -> t
-val remove : t -> IdPos.t -> t
 
 val check_clause : uf:IdPos.UF.t -> loc:Loc.t -> env:t -> ScopedTerm.t -> unit
 
 class merger : t -> object
   method get_all_func : t
   method get_local_func : t
-  method add_ty_abbr : Scope.type_decl_id -> ScopedTypeExpression.t -> unit
-  method add_func_ty_list : TypeList.t -> IdPos.t list -> unit
+  method add_ty_abbr : ScopedTypeExpression.t -> unit
   method merge : t
 end
