9dedf84f67112cf4f69aa553e8893ade3c590149
Davide Fissore <fisso9900@gmail.com>
2025-03-24 10:30:06 +0100

    more tests

--- tests/suite/correctness_FO.ml
+++ tests/suite/correctness_FO.ml
@@ -272,7 +272,8 @@ let () =
       Success; Success; Success; Success; Failure; (*50*)
       Success; Failure; Success; Failure; Failure; (*55*)
       Failure; Failure; Failure; Success; Success; (*60*)
-      Failure; Success; Success; Success; Failure
+      Failure; Success; Success; Success; Failure; (*65*)
+      Success; Failure
     |] in
   let ignore = [1;5;7;8;9;10;11;13;16;26;27] in
   for i = 0 to Array.length status - 1 do
