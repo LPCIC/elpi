2849b931564ae66164582e6d699f472d7ea89608
Enrico Tassi <Enrico.Tassi@Inria.fr>
2025-04-25 22:21:12 +0200

    fix error message

--- src/compiler/type_checker.ml
+++ src/compiler/type_checker.ml
@@ -399,7 +399,11 @@ let checker ~type_abbrevs ~kinds ~types:env ~unknown :
   and check_impl ~positive ctx ~loc ~tyctx b t1 t2 ety =
     if not @@ unify ety prop then error_bad_ety ~valid_mode ~loc ~tyctx ~ety:prop ScopedTerm.pretty_ (Impl(b,t1,t2)) (ety)
     else
-      let lhs, rhs,c,positive (* of => *) = if b <> R2L then t1,t2,F.implf,positive else t2,t1,F.rimplf,not positive in
+      let lhs, rhs,c,positive (* of => *) =
+        match b with
+        | L2R -> t1,t2,F.implf,positive
+        | L2RBang -> t1,t2,F.implbangf,positive
+        | R2L -> t2,t1,F.rimplf,not positive in
       let spills = check_loc ~positive ~tyctx:(Some c) ctx rhs ~ety:prop in
       let lhs_ty = mk_uvar "Src" in
       let more_spills = check_loc ~positive:(not positive) ~tyctx:None ctx ~ety:lhs_ty lhs in
